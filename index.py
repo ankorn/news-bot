f9764d09a7de8bd73246a1e27db691280a121da7
ant.korneev <ant.korneev@tinkoff.ru>
2024-06-04 10:46:28 +0400

    decomposition

--- index.py
+++ index.py
@@ -10,37 +10,75 @@ class Article:
     url: str = None
     title: str = None
     tag: str = None
+    text: str = None
     
+class MLStripper(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.reset()
+        self.strict = False
+        self.convert_charrefs= True
+        self.text = StringIO()
+    def handle_data(self, d):
+        self.text.write(d)
+    def get_data(self):
+        return self.text.getvalue()
 
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
+    
+def fetch(url):
+    time.sleep(np.random.choice([1, 2, 3]))
+    
+    try:
+        response = requests.get(url, headers={'User-Agent': UserAgent().chrome})
+        logger.info(response.text)
+    except Exception as error:
+        logger.error(error)
+        
+    return response
+    
+def parse_article(article_tree):
+    article = Article()
+    article.title = article_tree.h3.text
+    article.url = 'https://lenta.ru/' + article_tree.get('href')
+    article.tag = article_tree.find('span', {'class' : 'card-full-news__info-item card-full-news__rubric'}).get_text()
+    date = article_tree.find('time', {'class' : 'card-full-news__info-item card-full-news__date'}).get_text()
+    article.id = f'{article.title}{date}'
+    
+    response = fetch(article.url)
+    tree = BeautifulSoup(response.content)
+    
+    text_parts = tree.find_all('p', {'class' : 'topic-body__content-text'})
+    article.text = ''
+    
+    for text_part in text_parts:
+        article.text += strip_tags(text_part.get_text())
+    
+    return article
+        
+    
+def parse_article_list(index):
+    response = fetch(f'https://lenta.ru/parts/news/{index}/')
+        
+    tree = BeautifulSoup(response.content)
+        
+    articles_on_page = tree.find_all('a', {'class' : 'card-full-news _parts-news'})
+    tree.find()
+    articles_on_page_data = []
+    
+    for article in articles_on_page:
+        articles_on_page_data.append(parse_article(article))
+        
 def main():
+    # TODO: писать в файл
     articles_data = []
     previous_first_article_title
     i = 1
-    # TODO: сделать по аналогии с parse_page
-    # https://colab.research.google.com/drive/1VXAmkN9VcqQesUE9byNJyQ7l7gjFg4ZM#scrollTo=sV-n6jw9XP0A
     while True:
-        time.sleep(np.random.choice([3, 5, 7]))
-        
-        try:
-            response = requests.get(f'https://lenta.ru/parts/news/{i}/', headers={'User-Agent': UserAgent().chrome})
-            logger.info(response.text)
-        except Exception as error:
-            logger.error(error)
-            
-        tree = BeautifulSoup(response.content)
-            
-        articles_on_page = tree.find_all('a', {'class' : 'card-full-news _parts-news'})
-        articles_on_page_data = []
-        
-        for article in articles_on_page:
-            article_object = Article()
-            article_object.title = article.h3.text
-            article_object.url = 'https://lenta.ru/' + article.get('href')
-            article_object.tag = article.find_all('a', {'class' : 'card-full-news__info-item card-full-news__rubric'})
-            date = article.find_all('a', {'class' : 'card-full-news__info-item card-full-news__date'})
-            article_object.id = f'{article_object.title}{date}'
-            
-            articles_on_page_data.append(article_object)
+        articles_on_page_data = parse_article_list(i)
             
         if articles_on_page_data[0].title == previous_first_article_title or i > 10: # 50:
             break
